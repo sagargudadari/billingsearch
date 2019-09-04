a2a0c38dad99088d42a30d3c0a950fa4e6f50e32
Sagar Virupaxi Gudadari (Digital) <SA20091413@wipro.com>
2019-09-04 13:41:34 +0530

    error handling custmization

--- src/main/java/com/mastercard/billingsearch/exception/GlobalExceptionHandler.java
+++ src/main/java/com/mastercard/billingsearch/exception/GlobalExceptionHandler.java
@@ -1,25 +1,44 @@
 package com.mastercard.billingsearch.exception;
 
+import org.springframework.dao.DataAccessException;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.io.IOException;
 import java.util.Date;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
-@ControllerAdvice
-public class GlobalExceptionHandler {
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
-	@ExceptionHandler(ResourceNotFoundException.class)
-	public ResponseEntity<?> resourceNotFoundException(ResourceNotFoundException ex, WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
-	}
+    @ExceptionHandler({Exception.class, IOException.class, DataAccessException.class})
+    public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) {
+        ErrorDetails exceptionResponse = new ErrorDetails(new Date(), ex.getLocalizedMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
 
-	@ExceptionHandler(Exception.class)
-	public ResponseEntity<?> globalExceptionHandler(Exception ex, WebRequest request) {
-		ErrorDetails errorDetails = new ErrorDetails(new Date(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
-	}
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+                                                                  HttpHeaders headers,
+                                                                  HttpStatus status, WebRequest request) {
+        Map<String, Object> body = new LinkedHashMap<>();
+        body.put("status", status.value());
+
+        Map<String, String> fieldErrors = ex.getBindingResult().getFieldErrors().stream()
+                .collect(Collectors.toMap(FieldError::getField, FieldError::getDefaultMessage));
+
+        body.put("errors", fieldErrors);
+        body.put("timestamp", new Date());
+        return new ResponseEntity<>(body, headers, status);
+    }
 }
--- src/main/java/com/mastercard/billingsearch/model/SummaryModel.java
+++ src/main/java/com/mastercard/billingsearch/model/SummaryModel.java
@@ -3,18 +3,20 @@ package com.mastercard.billingsearch.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
-import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class SummaryModel {
 
-    @NotBlank
-    private String invoiceDate;
-    private String billingEvent;
-    private String invoiceNumber;
-    private String activityICA;
-    private String feederType;
-    private int page;
+    @NotNull
+    private final String invoiceDate;
+    private final String billingEvent;
+    private final String invoiceNumber;
+    private final String activityICA;
+    private final String feederType;
+    @Min(1)
+    private final int page;
 }
 
